12ba9bd34a2dd8882912cf129f81946e12436ceb
stellaorioneconsultant-sketch <stellaorioneconsultant@gmail.com>
2026-05-27 21:21:09 +0200

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,18 @@
+const CACHE = 'stella-orione-v3';
+const FILES = ['/stella-orione/stella-orione-pro%20(8).html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
