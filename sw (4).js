cbf59ad54e9976312a7e0133f3de0cb69a9e5ce0
stellaorioneconsultant-sketch <stellaorioneconsultant@gmail.com>
2026-05-29 22:25:21 +0200

    Add files via upload

---
+++ sw (4).js
@@ -0,0 +1,19 @@
+// Versione 10 - reset cache completo
+const CACHE = 'stella-orione-v10';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
