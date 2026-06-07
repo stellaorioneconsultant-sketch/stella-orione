ba4dd1c1a753154a347681293adee36cc8d9afea
stellaorioneconsultant-sketch <stellaorioneconsultant@gmail.com>
2026-06-07 21:19:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'so-v12';
+const CACHE = 'so-v13';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
