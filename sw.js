75584b85677988f427cb9f44cf5cb633fcfedf5d
stellaorioneconsultant-sketch <stellaorioneconsultant@gmail.com>
2026-06-24 22:59:41 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='so-v13c';
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request));});
