5e6e22ebde178e358d9d7e82db5b9c068a981576
stellaorioneconsultant-sketch <stellaorioneconsultant@gmail.com>
2026-09-02 23:13:11 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE="so-v17";
+self.addEventListener("install",()=>self.skipWaiting());
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request));});
